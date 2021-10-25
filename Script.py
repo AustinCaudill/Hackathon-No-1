f9ad440ad41327ab08cb3227f438f80413284a2a
Austin Caudill <31804903+AustinCaudill@users.noreply.github.com>
2021-10-25 15:41:22 -0500

    Added link data

--- Script.py
+++ Script.py
@@ -14,14 +14,30 @@ How do ads affect?
  """
 
 # Load Imports
+import os
 import pandas as pd
 from autoviz.AutoViz_Class import AutoViz_Class
 
-email_summary = pd.read_excel(r'summary.xlsx')
+# Load Data
+email_summary = pd.read_excel(r'summary.xlsx', parse_dates=[0])
 email_summary['Date'] = email_summary['Date/Time'].dt.date
 email_summary['Time'] = email_summary['Date/Time'].dt.time
+email_summary['Hour'] = email_summary['Date/Time'].dt.hour
+email_summary['Day'] = email_summary['Date/Time'].dt.day_name()
+
+files = os.listdir('link_data')
+link_data = pd.DataFrame() # Initialize dataframe
+for f in files:
+    data = pd.read_excel('./link_data/'+f, 'Sheet1')
+    link_data = link_data.append(data)
+
+# Need to cleanup by removing rows with "nan"
+link_data = link_data.dropna()
+
 
 # Perform EDA
+
+AV = AutoViz_Class()
 filename = "" # Not Needed
 dft = AV.AutoViz(
     filename,
@@ -33,3 +49,11 @@ dft = AV.AutoViz(
     lowess=True,
     chart_format="svg",
 )
+
+# Time to scrape previous emails
+dir = "https://thecolumn.co/daily/"
+files = listdir('dir')
+scraped_data = pd.DataFrame() # Initialize dataframe
+for f in files:
+    data = pd.read_excel('./link_data/'+f, 'Sheet1')
+    scraped_data = scraped_data.append(data)
