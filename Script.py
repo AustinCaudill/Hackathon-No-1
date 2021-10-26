d710ac8c999511d02d30e55983264f4b921ce360
Austin Caudill <31804903+AustinCaudill@users.noreply.github.com>
2021-10-26 10:18:28 -0500

    Cleaned URLs

--- Script.py
+++ Script.py
@@ -17,6 +17,20 @@ How do ads affect?
 import os
 import pandas as pd
 from autoviz.AutoViz_Class import AutoViz_Class
+import urllib3
+import certifi
+import validators
+from bs4 import BeautifulSoup
+
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 # Load Data
 email_summary = pd.read_excel(r'summary.xlsx', parse_dates=[0])
@@ -34,10 +48,25 @@ for f in files:
 # Need to cleanup by removing rows with "nan"
 link_data = link_data.dropna()
 
+bad_URLS = 0
+result = []
+# Need to remove bad URLs
+for l in link_data['Link']:
+    try:
+        test = validators.url(l)
+        print(test)
+        result.append(test)
+    except:
+        result.append("FAILED")
+
+link_data['Result'] = result
+link_data.loc[link_data['Result'] == True]
+
+
 
 # Perform EDA
 
-AV = AutoViz_Class()
+""" AV = AutoViz_Class()
 filename = "" # Not Needed
 dft = AV.AutoViz(
     filename,
@@ -48,12 +77,14 @@ dft = AV.AutoViz(
     verbose=1,
     lowess=True,
     chart_format="svg",
-)
+) """
 
-# Time to scrape previous emails
-dir = "https://thecolumn.co/daily/"
-files = listdir('dir')
-scraped_data = pd.DataFrame() # Initialize dataframe
-for f in files:
-    data = pd.read_excel('./link_data/'+f, 'Sheet1')
-    scraped_data = scraped_data.append(data)
+# Time to scrape link data
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
+for link in link_data['Link']:
+    print("Getting:", link)
+    r2 = http.request('GET', link, headers=headers, retries=urllib3.Retry(redirect=2, raise_on_redirect=False))
+    if r2.status != 200: 
+     continue
+    soup2 = BeautifulSoup(r2.read(), "html.parser")
