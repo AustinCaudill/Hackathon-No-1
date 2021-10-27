775669d4fef44954ca8018fe1d8d67eb49b9364c
Austin Caudill <31804903+AustinCaudill@users.noreply.github.com>
2021-10-27 10:05:46 -0500

    Cleaned up preprocessing

--- Script.py
+++ Script.py
@@ -21,6 +21,17 @@ import urllib3
 import certifi
 import validators
 from bs4 import BeautifulSoup
+from nltk import ngrams
+from collections import Counter
+import seaborn as sns
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 
 import ssl
 try:
@@ -31,6 +42,7 @@ except AttributeError:
 else:
     # Handle target environment that doesn't support HTTPS verification
     ssl._create_default_https_context = _create_unverified_https_context
+
 
 # Load Data
 email_summary = pd.read_excel(r'summary.xlsx', parse_dates=[0])
@@ -61,9 +73,9 @@ for l in link_data['Link']:
 
 link_data['Result'] = result
 cleaned_URLs = link_data.loc[link_data['Result'] == True]
-cleaned_URLs = cleaned_URLs[~cleaned_URLs.Link.str.contains('pdf|jpg')]
+cleaned_URLs = cleaned_URLs[~cleaned_URLs.Link.str.contains('pdf|jpg|jpeg|JPG|png|cgi')]
 
-cleaned_URLs = cleaned_URLs.loc[cleaned_URLs['Clicks'] > 20]
+cleaned_URLs = cleaned_URLs.loc[cleaned_URLs['Clicks'] > 80]
 
 
 
@@ -103,45 +115,44 @@ for link in cleaned_URLs['Link']:
 
 
 # Text Preprocessing
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import re
 stop_words = set(stopwords.words('english'))
 stop_words_ext = ['n', 'please', 'nthe', 'license', 'cc', 'nmore', 'xa', 'c', 'u', 'r', 'f', 'licensing','licensed', 'licenses', 'creative commons','used','copyright'] # custom word exclusion list.
 
-
 def preprocess(text):
-    
-    #regular expression keeping only letters - more on them later
-    letters_only_text = re.sub("[^a-zA-Z]", " ", text)
-    letters_only_text  = re.sub("'", "", letters_only_text)
 
-    # convert to lower case and split into words -> convert string into list ( 'hello world' -> ['hello', 'world'])
-    words = letters_only_text.lower().split()
+    new_tokens = word_tokenize(text)
+    new_tokens = [t for t in new_tokens if t.isalpha()]
+    new_tokens = [t.lower() for t in new_tokens]
+
+    lemmatizer = WordNetLemmatizer()
+    new_tokens =[lemmatizer.lemmatize(t) for t in new_tokens]
 
     cleaned_words = []
-    
     # remove stopwords
-    for word in words:
+    for word in new_tokens:
         if word not in stop_words and word not in stop_words_ext:
             cleaned_words.append(word)
-            
-    cleaned_words = [i.replace("'", "") for i in cleaned_words]      
+    
+    #counts the words, pairs and trigrams
+    counted = Counter(cleaned_words)
+    counted_2= Counter(ngrams(cleaned_words,2))
+    counted_3= Counter(ngrams(cleaned_words,3))
+    #creates 3 data frames and returns thems
+    word_freq = pd.DataFrame(counted.items(),columns=['word','frequency']).sort_values(by='frequency',ascending=False)
+    word_pairs =pd.DataFrame(counted_2.items(),columns=['pairs','frequency']).sort_values(by='frequency',ascending=False)
+    trigrams =pd.DataFrame(counted_3.items(),columns=['trigrams','frequency']).sort_values(by='frequency',ascending=False)
 
-    return cleaned_words
+    return cleaned_words,word_freq,word_pairs,trigrams
 
 soupey = str(soupey)
-cleaned_soupey = preprocess(soupey)    
+cleaned_soupey,word_freq,word_pairs,trigrams = preprocess(soupey)    
 
-word_tokens = word_tokenize(str(cleaned_soupey)) #converts into individual words
-
-number_of_words = len(word_tokens)
+number_of_words = len(cleaned_soupey)
 print(number_of_words)
 
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
 
-wordcloud = WordCloud(width = 700, height = 700, background_color ='white', min_font_size = 10).generate(str(word_tokens) )
+cleaned_soupey = " ".join(cleaned_soupey)
+wordcloud = WordCloud(width = 700, height = 700, background_color ='white', min_font_size = 10).generate((cleaned_soupey))
   
 # plot the WordCloud image                        
 plt.figure(figsize = (10, 10), facecolor = None) 
@@ -151,4 +162,14 @@ plt.tight_layout(pad = 0)
 plt.show()
 plt.savefig('test.png')
 
-print("finished")
+
+# create subplot of the different data frames
+fig, axes = plt.subplots(3,1,figsize=(8,20))
+sns.barplot(ax=axes[0],x='frequency',y='word',data=word_freq.head(30))
+sns.barplot(ax=axes[1],x='frequency',y='pairs',data=word_pairs.head(30))
+sns.barplot(ax=axes[2],x='frequency',y='trigrams',data=trigrams.head(30))
+
+
+
+
+print("Script Finished")
