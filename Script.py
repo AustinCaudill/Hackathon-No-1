d341d682e44e08b14f20f36149c6a2f4cb414f3e
Austin Caudill <31804903+AustinCaudill@users.noreply.github.com>
2021-10-26 19:29:58 -0500

    Updated

--- Script.py
+++ Script.py
@@ -60,7 +60,10 @@ for l in link_data['Link']:
         result.append("FAILED")
 
 link_data['Result'] = result
-link_data.loc[link_data['Result'] == True]
+cleaned_URLs = link_data.loc[link_data['Result'] == True]
+cleaned_URLs = cleaned_URLs[~cleaned_URLs.Link.str.contains('pdf|jpg')]
+
+cleaned_URLs = cleaned_URLs.loc[cleaned_URLs['Clicks'] > 20]
 
 
 
@@ -79,12 +82,73 @@ dft = AV.AutoViz(
     chart_format="svg",
 ) """
 
+
 # Time to scrape link data
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
-for link in link_data['Link']:
-    print("Getting:", link)
-    r2 = http.request('GET', link, headers=headers, retries=urllib3.Retry(redirect=2, raise_on_redirect=False))
-    if r2.status != 200: 
-     continue
-    soup2 = BeautifulSoup(r2.read(), "html.parser")
+soupey = []
+for link in cleaned_URLs['Link']:
+    try:
+        print("Getting:", link)
+        req = http.request('GET', link, headers=headers, retries=urllib3.Retry(redirect=2, raise_on_redirect=False))
+        if req.status != 200: 
+            continue
+        soup = BeautifulSoup(req.data, "html.parser")
+        text = soup.get_text()
+        print(text)
+        soupey.append(text)
+    except:
+        print('failed')
+        continue
+
+
+# Text Preprocessing
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
+stop_words = set(stopwords.words('english'))
+stop_words_ext = ['n', 'please', 'nthe', 'license', 'cc', 'nmore', 'xa', 'c', 'u', 'r', 'f', 'licensing','licensed', 'licenses', 'creative commons','used','copyright'] # custom word exclusion list.
+
+
+def preprocess(text):
+    
+    #regular expression keeping only letters - more on them later
+    letters_only_text = re.sub("[^a-zA-Z]", " ", text)
+    letters_only_text  = re.sub("'", "", letters_only_text)
+
+    # convert to lower case and split into words -> convert string into list ( 'hello world' -> ['hello', 'world'])
+    words = letters_only_text.lower().split()
+
+    cleaned_words = []
+    
+    # remove stopwords
+    for word in words:
+        if word not in stop_words and word not in stop_words_ext:
+            cleaned_words.append(word)
+            
+    cleaned_words = [i.replace("'", "") for i in cleaned_words]      
+
+    return cleaned_words
+
+soupey = str(soupey)
+cleaned_soupey = preprocess(soupey)    
+
+word_tokens = word_tokenize(str(cleaned_soupey)) #converts into individual words
+
+number_of_words = len(word_tokens)
+print(number_of_words)
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+wordcloud = WordCloud(width = 700, height = 700, background_color ='white', min_font_size = 10).generate(str(word_tokens) )
+  
+# plot the WordCloud image                        
+plt.figure(figsize = (10, 10), facecolor = None) 
+plt.imshow(wordcloud) 
+plt.axis("off") 
+plt.tight_layout(pad = 0) 
+plt.show()
+plt.savefig('test.png')
+
+print("finished")
